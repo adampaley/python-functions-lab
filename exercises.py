34fdb674468a1c12910853428b648ee2ee23d2b8
Adam Paley <apaley95@gmail.com>
2025-02-15 20:58:21 -0500

    completed lab exercise 3

--- exercises.py
+++ exercises.py
@@ -48,3 +48,30 @@ def simple_interest (principal, rate, time):
 
 print('Exercise 2:', simple_interest(1000, 5, 2))
 print('Exercise 2:', simple_interest(1500, 3.5, 5))
+
+# Exercise 3: Apply a Discount
+#
+# Write a function named `apply_discount` that takes a product's price and a discount percentage (from 0 to 100).
+# Return the new price after applying the discount.
+#
+# Examples:
+# apply_discount(100, 25) should return 75.
+# apply_discount(80, 10) should return 72.
+#
+# Define your function and call it to display the discounted price.
+
+def apply_discount (price, discount):
+    try:
+        price = float(price)
+        discount = float(discount)/100
+        if price <= 0 or discount <= 0:
+            return print("Invalid Input: Must enter positive numeric inputs.")
+        elif discount > 100:
+            return print("Invalid Input: Price cannot be discounted beyond free.")
+        new_price = (price - (price*discount))
+        return f'${int(new_price)}' if new_price.is_integer() else f'${new_price}'
+    except ValueError:
+        print("ValueError: Must enter numeric inputs.")
+
+print('Exercise 3:', apply_discount(100, 25))
+print('Exercise 3:', apply_discount(80, 10))
