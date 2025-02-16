25a8e26e7e064132bd7755aa0fa8f5e14ae3f56f
Adam Paley <apaley95@gmail.com>
2025-02-15 20:30:36 -0500

    completed lab exercise 1

---
+++ exercises.py
@@ -0,0 +1,21 @@
+# Exercise 1: Calculate Area of a Triangle
+#
+# Write a function named `calculate_area_triangle` that takes the base and height of a triangle and returns the area.
+# The area formula is (base * height) / 2.
+#
+# Examples:
+# calculate_area_triangle(10, 5) should return 25.0.
+# calculate_area_triangle(7, 3) should return 10.5.
+#
+# Define your function and call it below.
+
+def calculate_area_triangle(base, height):
+    try:
+        base = float(base)
+        height = float(height)
+        return base * height / 2
+    except TypeError:
+        print("TypeError: Must enter numeric inputs.")
+
+print('Exercise 1:', calculate_area_triangle(10, 5))
+print('Exercise 1:', calculate_area_triangle(7, 3))
